f188419d443b2464e17ce378dad96fbbafacd319
Antonescu Andrei-Iosif <78093165+Andrei25i@users.noreply.github.com>
2025-05-06 00:28:30 +0300

    Changed from exec to execFile (child_process) because exec is deprecated

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { exec } = require("child_process");
+const { execFile } = require("child_process");
 const path = require("path");
 const cors = require("cors");
 
@@ -16,7 +16,8 @@ app.get("/get-audio", async (req, res) => {
   if (!url) return res.status(400).json({ error: "Missing URL" });
 
   const cmd = `"${ytdlpPath}" -f bestaudio -g "${url}"`;
-  exec(cmd, (error, stdout, stderr) => {
+  const args = ['-f', 'bestaudio', '-g', url];
+  execFile(ytdlpPath, args, (error, stdout, stderr) => {
     if (error) {
       console.error(`Error: ${stderr}`);
       return res.status(500).send("Something went wrong...");
