84342b39c7f13be9a3cfc0867b72cba07b646746
Antonescu Andrei-Iosif <78093165+Andrei25i@users.noreply.github.com>
2025-05-05 23:48:26 +0300

    Changed from distube/ytdl-core to yt_dlp

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
-const ytdl = require('@distube/ytdl-core');
+const { exec } = require("child_process");
+const path = require("path");
 const cors = require("cors");
 
 const app = express();
@@ -7,29 +8,24 @@ const port = 3000;
 
 app.use(cors());
 
+const ytdlpPath = path.join(__dirname, "yt_env", "Scripts", "yt-dlp.exe");
+
 app.get("/get-audio", async (req, res) => {
-    const url = req.query.url;
+  const url = req.query.url;
 
-    if (!url || !ytdl.validateURL(url)) {
-        return res.status(400).json({ error: "Invalid or missing URL" });
+  if (!url) return res.status(400).json({ error: "Missing URL" });
+
+  const cmd = `"${ytdlpPath}" -f bestaudio -g "${url}"`;
+  exec(cmd, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Error: ${stderr}`);
+      return res.status(500).send("Something went wrong...");
     }
 
-    try {
-        const info = await ytdl.getInfo(url);
-
-        // (sometimes audio sources return error 403, so video sources work better):
-        const bestFormat = ytdl.chooseFormat(info.formats, {quality: '18'}) // the only itag that has audio and video at the best quality
-        if (!bestFormat) {
-            return res.status(404).json({ error: "Desired format not found" });
-        }
-        res.json({ audioUrl: bestFormat.url });
-
-    } catch (error) {
-        res.status(500).json({ error: "Could not extract audio" });
-        console.log(error);
-    }
+    res.json({ audioUrl: stdout.trim() });
+  });
 });
 
 app.listen(port, () => {
-    console.log(`Server runs on http://localhost:${port}`);
+  console.log(`Server runs on http://localhost:${port}`);
 });
